cf29cb488d06cdf59608168154a3fce1b784c57a
dzup4uk <vitya_d@ukr.net>
2019-01-15 23:39:34 +0200

    Update get_version.js

--- get_version.js
+++ get_version.js
@@ -12,3 +12,19 @@ function getText(){
         } 
     } 
 }
+function populateTables(){
+    
+    var outer_text = getText();
+    outer_text = outer_text.split('\n');    // you can adjust the manner of parsing the received file (regexp)
+    
+    var tableHP = document.getElementById("tHP");
+// Create an empty <tr> element and add it to the 1st position of the table:
+    var row = tableHP.insertRow(tableHP.rows.length);
+// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+// Add some text to the new cells:
+    cell1.innerHTML = outer_text[0];
+    cell2.innerHTML = outer_text[1];
+}
