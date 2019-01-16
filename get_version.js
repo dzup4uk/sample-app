f2c77205202dcc7e3639e26464e28707d7f4fb28
dzup4uk <vitya_d@ukr.net>
2019-01-16 13:37:19 +0200

    Update get_version.js

--- get_version.js
+++ get_version.js
@@ -1,30 +1,10 @@
-function getText(){ 
-    // read text from URL location 
-    var request = new XMLHttpRequest(); 
-    request.open('GET', protocol//hostname:port/'version.txt', true); 
-    request.send(null); 
-    request.onreadystatechange = function () { 
-        if (request.readyState === 4 && request.status === 200) { 
-            var type = request.getResponseHeader('Content-Type'); 
-            if (type.indexOf("text") !== 1) { 
-                return request.responseText; 
-            } 
-        } 
-    } 
-}
-function populateTables(){
-    
-    var outer_text = getText();
-    outer_text = outer_text.split('\n');    // you can adjust the manner of parsing the received file (regexp)
-    
-    var tableHP = document.getElementById("tHP");
-// Create an empty <tr> element and add it to the 1st position of the table:
-    var row = tableHP.insertRow(tableHP.rows.length);
-// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-
-// Add some text to the new cells:
-    cell1.innerHTML = outer_text[0];
-    cell2.innerHTML = outer_text[1];
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementbyId("demo").innerHTML = this.responseText;
+        }
+    };
+xhttp.open("GET", "version.txt", true);
+xhttp.send();
 }
